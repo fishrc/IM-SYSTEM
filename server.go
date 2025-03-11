a5761c0506c9c931e4c729d10b61e19e1622256e
fishrc <shuqinkissshot@gmail.com>
2025-03-11 19:42:58 +0800

    add user module

--- server.go
+++ server.go
@@ -3,23 +3,59 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// online users list
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// broadcast channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	return &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
+}
+
+// listen Message and broadcast to all users
+func (s *Server) ListenMessage() {
+	for {
+		msg := <-s.Message
+
+		s.mapLock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
+}
+
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "] " + user.Name + ": " + msg + "\n"
+	s.Message <- sendMsg
 }
 
 func (s *Server) Handler(conn net.Conn) {
 	// handle connection
-	fmt.Println("new connection created")
+	// fmt.Println("new connection created")
+	user := NewUser(conn)
+	// add new online user to list
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	// broadcast new online user
+	s.BroadCast(user, "is online")
 }
 
 func (s *Server) Start() {
@@ -29,6 +65,8 @@ func (s *Server) Start() {
 		fmt.Println("listen error:", err)
 	}
 	defer listener.Close()
+
+	go s.ListenMessage()
 
 	for {
 		// accept
